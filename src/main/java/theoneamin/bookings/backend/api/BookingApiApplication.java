8990960b47e2c2859d98cbbb0ca15808ba56be80
mamin11 <abdimamin@gmail.com>
2022-05-28 09:32:13 +0100

    initial setup

---
+++ src/main/java/theoneamin/bookings/backend/api/BookingApiApplication.java
@@ -0,0 +1,13 @@
+package theoneamin.bookings.backend.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingApiApplication.class, args);
+	}
+
+}
